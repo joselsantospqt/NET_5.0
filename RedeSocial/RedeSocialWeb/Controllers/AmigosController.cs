8b33dd3f1164a600c57b0628827785be61fb1eea
Your Name <you@example.com>
2021-09-12 19:56:04 -0300

    Layout web; inclusão de controller, configuração de rotas, js, cs, plugins, imagens, views.

---
+++ RedeSocial/RedeSocialWeb/Controllers/AmigosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedeSocialWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedeSocialWeb.Controllers
+{
+    public class AmigosController : Controller
+    {
+        public IActionResult TodosAmigos()
+        {
+            return View();
+        }
+    }
+}
---
+++ RedeSocial/RedeSocialWeb/Controllers/PerfilController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedeSocialWeb.Controllers
+{
+    public class PerfilController : Controller
+    {
+        public IActionResult EditarPerfil()
+        {
+            return View();
+        } 
+
+        public IActionResult ExcluirConta()
+        {
+            return View();
+        } 
+        
+        public IActionResult PerfilExterno()
+        {
+            return View();
+        } 
+        
+        public IActionResult PerfilUsuario()
+        {
+            return View();
+        } 
+        
+
+    }
+}
