8a30425b44d591abdbc68403378bfb519b503722
Raul <raulpires224@gmail.com>
2021-09-14 20:00:03 -0300

    Realizado junto ao José o ajuste na relação de comentários para ter um autor. Também feito o cadastro de usuario e pré arrumada a funcionalidade de login, iniciado o controller. Conseguindo listar todos os posts da index porém sem os comentários ainda.

--- RedeSocial/Domain/Entidade/Comment.cs
+++ RedeSocial/Domain/Entidade/Comment.cs
@@ -5,17 +5,23 @@ namespace Domain.Entidade
 {
     public class Comment
     {
-        public Comment(){Post = new PostComment();}
+        public Comment(){Post = new PostComment(); Pessoa = new CommentPessoa(); }
         [Key]
         public Guid Id { get; set; }
         public string Text{ get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpDateTime { get; set; }
         public PostComment Post { get; set; }
+        public CommentPessoa Pessoa { get; set; }
 
         internal void AddPost(Guid postId)
         {
             Post = new PostComment() { PostId = postId, CommentId = this.Id };
+        }
+
+        internal void AddPessoa(Guid pessoaId)
+        {
+            Pessoa = new CommentPessoa() { PessoaId = pessoaId, CommentId = this.Id };
         }
     }
 }
