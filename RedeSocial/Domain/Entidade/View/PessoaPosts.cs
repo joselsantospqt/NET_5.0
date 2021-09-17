eac7bc5222bba14453dc93cbd4de9df0dd7841b6
José Ricardo <jose.lsantos@al.infnet.edu.br>
2021-09-17 04:07:49 -0300

    Exemplo BlobStorage Funcionando

---
+++ RedeSocial/Domain/Entidade/View/PessoaPosts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entidade.View
+{
+    public class PessoaPosts
+    {
+        public Pessoa pessoa { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
