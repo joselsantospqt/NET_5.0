12791c1d755f29d2ef7a682b07fde155da23086a
Raul <raulpires224@gmail.com>
2021-09-20 22:34:29 -0300

    Ajustes nas views de amigos, layout e index

--- RedeSocial/RedeSocialWeb/Controllers/AmigosController.cs
+++ RedeSocial/RedeSocialWeb/Controllers/AmigosController.cs
@@ -1,34 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Entidade;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using RedeSocialWeb.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace RedeSocialWeb.Controllers
 {
-    public class AmigosController : Controller
+    public class AmigosController : BaseController
     {
-        //buscar todos as pessoas
-        public IActionResult View()
+        private readonly ILogger<HomeController> _logger;
+        public AmigosController(IHttpClientFactory httpClientFactory, IConfiguration configuration, ILogger<HomeController> logger) : base(httpClientFactory, configuration)
         {
-            return View();
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var retorno = await ApiFind<Pessoa>(this.HttpContext.Session.GetString("Token"), "Pessoas");
+
+            return View(retorno);
         }
 
 
-        //buscar todos as pessoa por id
-        [HttpPost("id:Guid")]
-        public IActionResult View(Guid id)
+        //buscar a pessoa por id
+        public async Task<IActionResult> Details(Guid id)
         {
-            return View("Details");
+            var pessoa = await ApiFindById<Pessoa>(this.HttpContext.Session.GetString("Token"), id ,"Pessoas");
+
+            return View(pessoa);
         }
 
-        [HttpPost]
-        public IActionResult Adicionar()
+        public async Task<IActionResult> Adicionar(Guid id)
         {
-            return View();
+            await ApiSaveAutorize(this.HttpContext.Session.GetString("Token"), 
+                new { idPessoa = this.HttpContext.Session.GetString("UserId"), idAmigo = id }, "Pessoas");
+
+            return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> View()
+        {
+            var retorno = await ApiFindById<Pessoa>(this.HttpContext.Session.GetString("Token"), this.HttpContext.Session.GetString("UserId"), "Pessoas");
+
+            //var listId = 
+
+            return View(retorno);
         }
 
     }
